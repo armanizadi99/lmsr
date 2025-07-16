290f176aefa31e4f29e58c9ed4d49ad484ca9369
armanizadi99 <armanizadi9999@gmail.com>
2025-07-16 11:13:06 +0330

    added a few domain exceptions.

--- src/Lmsr.Domain/common/DomainExceptions.cs
+++ src/Lmsr.Domain/common/DomainExceptions.cs
@@ -8,3 +8,13 @@ public class ValidationException : DomainException
 {
 public ValidationException (string message) : base(message);
 }
+
+public class DomainRouleViolationException : DomainException
+{
+public DomainRouleViolationException(string message) : base(message);
+}
+
+public class InvalidDomainOperationException : DomainException
+{
+public InvalidDomainOperationException(string message) : base(message);
+}
