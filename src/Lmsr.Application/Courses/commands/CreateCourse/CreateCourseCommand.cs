bd3415fd6bd46f284ab384a6009fc4442e8a020d
armanizadi99 <armanizadi9999@gmail.com>
2025-08-02 12:32:48 +0330

    changed the command fields to properties.

--- src/Lmsr.Application/Courses/commands/CreateCourse/CreateCourseCommand.cs
+++ src/Lmsr.Application/Courses/commands/CreateCourse/CreateCourseCommand.cs
@@ -1,7 +1,7 @@
 ﻿namespace Lmsr.Application.Courses;
 public class CreateCourseCommand (string title, string userId, bool isPrivate) : IRequest<Result<int>>
 {
-public string Title = title;
-public string UserId = userId;
-public bool IsPrivate = isPrivate;
+public string Title{get; private set; } = title;
+public string UserId{get; private set; } = userId;
+public bool IsPrivate {get; private set; } = isPrivate;
 }
