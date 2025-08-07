541868d373ba3860777749853656c399295e50bb
armanizadi99 <armanizadi9999@gmail.com>
2025-08-07 11:07:52 +0330

    Modified two CreateCourse test cases so the changes are reflected.

--- tests/Lmsr.Application.Tests/Courses/Commands/CreateCourse/CreateCourseHandlerTests.cs
+++ tests/Lmsr.Application.Tests/Courses/Commands/CreateCourse/CreateCourseHandlerTests.cs
@@ -16,21 +16,21 @@ public async Task Handle_ValidCommand_ShouldMakeACourse()
 {
 // Arrange
 var mockTitleUniquenessSpec = new Mock<ICourseTitleUniquenessSpecification>();
-var mockUserIdAuthenticitySpec = new Mock<ICourseUserIdAuthenticitySpecification>();
 var mockUnitOfWork = new Mock<IUnitOfWork>();
+var mockUserContext = new Mock<IUserContext>();
 var mockCourseRepo = new Mock<ICourseRepository>();
+var userId = Guid.NewGuid().ToString();
 Course capturedCourse = null;
-mockUserIdAuthenticitySpec.Setup( m => m.IsUserIdAuthentic(It.IsAny<string>())).Returns(true);
 mockTitleUniquenessSpec.Setup(m => m.IsTitleUnique(It.IsAny<string>())).Returns(true);
 mockCourseRepo.Setup(m => m.AddAsync(It.IsAny<Course>()))
 .Callback<Course>(c => {c.Id=1; capturedCourse = c;})
 .Returns(Task.CompletedTask);
 mockUnitOfWork.Setup(m => m.CourseRepo).Returns(mockCourseRepo.Object);
+mockUserContext.Setup(m => m.UserId).Returns(userId);
 var title = "valid title";
-var userId = Guid.NewGuid().ToString();
 var isPrivate = false;
-CreateCourseHandler handler = new CreateCourseHandler(mockUnitOfWork.Object, mockTitleUniquenessSpec.Object, mockUserIdAuthenticitySpec.Object);
-CreateCourseCommand command = new CreateCourseCommand(title, userId, isPrivate);
+CreateCourseHandler handler = new CreateCourseHandler(mockUnitOfWork.Object, mockTitleUniquenessSpec.Object, mockUserContext.Object);
+CreateCourseCommand command = new CreateCourseCommand(title, isPrivate);
 
 // Act
 var result = await handler.Handle(command, CancellationToken.None);
@@ -45,36 +45,18 @@ mockUnitOfWork.Verify(m => m.SaveChangesAsync(), Times.Once);
 }
 
 [Fact]
-public async Task Handle_InauthenticUserId_ShouldThrowInvalidDomainOperationException()
-{
-// Arrange
-var mockUserIdAuthenticitySpec = new Mock<ICourseUserIdAuthenticitySpecification>();
-var mockTitleUniquenessSpec = new Mock<ICourseTitleUniquenessSpecification>();
-var mockUnitOfWork = new Mock<IUnitOfWork>();
-mockUserIdAuthenticitySpec.Setup(m => m.IsUserIdAuthentic("123")).Returns(false);
-var handler = new CreateCourseHandler(mockUnitOfWork.Object, mockTitleUniquenessSpec.Object, mockUserIdAuthenticitySpec.Object);
-var command = new CreateCourseCommand("course1", "123", false);
-
-// Act
-Func<Task> act = () => handler.Handle(command, CancellationToken.None);
-
-// Assert
-await act.Should().ThrowAsync<InvalidDomainOperationException>()
-.WithMessage("Invalid UserId. ");
-}
-
-[Fact]
 public async Task Handle_DuplicateTitle_ShouldReturnError()
 {
 // Arrange
-var mockUserIdAuthenticitySpec = new Mock<ICourseUserIdAuthenticitySpecification>();
 var mockTitleUniquenessSpec = new Mock<ICourseTitleUniquenessSpecification>();
+var mockUserContext = new Mock<IUserContext>();
 var mockUnitOfWork = new Mock<IUnitOfWork>();
+var userId = Guid.NewGuid().ToString();
 var duplicateTitle = "course1";
-mockUserIdAuthenticitySpec.Setup(m => m.IsUserIdAuthentic(It.IsAny<string>())).Returns(true);
 mockTitleUniquenessSpec.Setup(m => m.IsTitleUnique(duplicateTitle)).Returns(false);
-var handler = new CreateCourseHandler(mockUnitOfWork.Object, mockTitleUniquenessSpec.Object, mockUserIdAuthenticitySpec.Object);
-var command = new CreateCourseCommand(duplicateTitle, "123", false);
+mockUserContext.Setup(m => m.UserId).Returns(userId);
+var handler = new CreateCourseHandler(mockUnitOfWork.Object, mockTitleUniquenessSpec.Object, mockUserContext.Object);
+var command = new CreateCourseCommand(duplicateTitle, false);
 
 // Act
 var result = await handler.Handle(command, CancellationToken.None);
