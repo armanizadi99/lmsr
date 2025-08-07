80bd4e6d268b310f46d7ddca46cea257e2c37633
armanizadi99 <armanizadi9999@gmail.com>
2025-08-07 09:59:29 +0330

    Added NotAuthorized to the error codes.

--- src/Lmsr.Domain/common/ErrorCodes.cs
+++ src/Lmsr.Domain/common/ErrorCodes.cs
@@ -4,4 +4,5 @@ public static class ErrorCodes
 public static string ValidationError = "Validation error";
 public static string NotFound = "Not found";
 public static string DuplicateEntity = "Duplicate entity";
+public static string NotAuthorized = "Not authorized";
 }
