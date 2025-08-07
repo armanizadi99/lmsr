2203d3d5ba29719a4f73262e5e76c03ce95d69ef
armanizadi99 <armanizadi9999@gmail.com>
2025-08-07 10:05:02 +0330

    Added the DeleteCourseCommand to the Di container.

--- src/Lmsr.Application/DependencyInjection.cs
+++ src/Lmsr.Application/DependencyInjection.cs
@@ -10,7 +10,9 @@ public static class DependencyInjection
 public static IServiceCollection AddApplication(this IServiceCollection services)
 {
 services.AddScoped<IRequestHandler<CreateCourseCommand, Result<int>>, CreateCourseHandler>();
+services.AddScoped<IRequestHandler<DeleteCourseCommand, Result>, DeleteCourseHandler>();
 services.AddScoped<IRequestHandler<GetAllCoursesQuery, Result<List<CourseViewModel>>>, GetAllCoursesHandler>();
+
 services.AddValidatorsFromAssemblyContaining<CreateCourseHandler>();
 return services;
 }
