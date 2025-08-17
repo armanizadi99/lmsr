b5d9bf6ac84f818cb9439164ce2634baa0c8a1d9
armanizadi99 <armanizadi9999@gmail.com>
2025-08-17 13:28:06 +0330

    changed the result types factory method to take a single error instead of a list.

--- src/Lmsr.Domain/common/ResultType.cs
+++ src/Lmsr.Domain/common/ResultType.cs
@@ -3,17 +3,17 @@ public class Result<T>
 {
 public bool IsSuccess { get; private set; }
 public T Value { get; private set; }
-public List<DomainError> Errors { get; private set;}
+public DomainError Error { get; private set;}
 
 public static Result<T> Success(T value) => new Result<T> { IsSuccess = true, Value = value };
-public static Result<T> Failure(List<DomainError> errors) => new Result<T> { IsSuccess = false, Errors = errors };
+public static Result<T> Failure(DomainError error) => new Result<T> { IsSuccess = false, Error = error };
 }
 
 public class Result
 {
 public bool IsSuccess { get;  private set; }
-public List<DomainError> Errors { get;  private set; }
+public DomainError Error { get;  private set; }
 
 public static Result Success() => new Result { IsSuccess = true};
-public static Result Failure(List<DomainError> errors) => new Result { IsSuccess = false, Errors = errors };
+public static Result Failure(DomainError error) => new Result { IsSuccess = false, Error = error };
 }
